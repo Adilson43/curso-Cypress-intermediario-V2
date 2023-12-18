bc6eb9b609c2931380e8981319a82a8859d8c48d
adilson.oliveira <adilson.oliveira@crmbonus.com>
2023-12-18 13:56:24 -0300

    criação de teste login 18.12.2023

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -22,16 +22,24 @@ Cypress.Commands.add('login', (
   }
 })
 
-  Cypress.Commands.add('logout', () => {
-    cy.get('.qa-user-avatar').click()
-    cy.contains('Sign out').click()
-  })
+Cypress.Commands.add('logout', () => {
+  cy.get('.qa-user-avatar').click()
+  cy.contains('Sign out').click()
+})
 
-  Cypress.Commands.add('gui_createProject', project => {
-    cy.visit('http://localhost:81/projects/new')
-  
-    cy.get('#project_name').type(project.name)
-    cy.get('#project_description').type(project.description)
-    cy.get('.qa-initialize-with-readme-checkbox').check()
-    cy.contains('Create project').click()
-  })
+Cypress.Commands.add('gui_createProject', project => {
+  cy.visit('http://localhost:81/projects/new')
+
+  cy.get('#project_name').type(project.name)
+  cy.get('#project_description').type(project.description)
+  cy.get('.qa-initialize-with-readme-checkbox').check()
+  cy.contains('Create project').click()
+})
+
+Cypress.Commands.add('gui_createIssue', issue => {
+  cy.visit(`http://localhost:81/${Cypress.env('user_name')}/${issue.project.name}/issues/new`)
+
+  cy.get('.qa-issuable-form-title').type(issue.title)
+  cy.get('.qa-issuable-form-description').type(issue.description)
+  cy.contains('Submit issue').click()
+})
