08b245d1d19e71cc5ef91e4c39165f48eb330b6f
adilson.oliveira <adilson.oliveira@crmbonus.com>
2023-12-19 14:27:04 -0300

    Aula cypress intermed 19.12.2023

--- cypress.config.js
+++ cypress.config.js
@@ -3,17 +3,11 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:81/',
+    env: {
+      hideCredentials: true,
+      requestMode: true,
+    },
   },
-
   fixturesFolder: false,
   video: false,
-});
-
-module.exports = {
-  e2e: {
-    supportFile: 'cypress/support/e2e.js',
-  },
-  component: {
-    supportFile: 'cypress/support/component.js',
-  },
-};
+})
